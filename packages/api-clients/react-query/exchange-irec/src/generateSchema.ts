1e93003b68643c2ce7e1bf6423cb67494af514ef
Nicholas Goorwah <Nick@VolterraTechnology.com>
2022-03-01 17:44:51 -0500

    Azure Changes for config to work with external database

--- packages/api-clients/react-query/exchange-irec/src/generateSchema.ts
+++ packages/api-clients/react-query/exchange-irec/src/generateSchema.ts
@@ -17,10 +17,10 @@ export const generateSchema = async () => {
         imports: [
             TypeOrmModule.forRoot({
                 type: 'postgres',
-                host: process.env.DB_HOST ?? 'localhost',
-                port: parseInt(process.env.DB_PORT) || 5432,
-                username: process.env.DB_USERNAME ?? 'postgres',
-                password: process.env.DB_PASSWORD ?? 'postgres',
+                host: process.env.DB_HOST ?? 'kyanite-server.postgres.database.azure.com',
+                port: Number(process.env.DB_PORT) ?? 5432,
+                username: process.env.DB_USERNAME ?? 'lvcddedcrc',
+                password: process.env.DB_PASSWORD ?? '3N8U0QL1L35L43JR$',
                 database: process.env.DB_DATABASE ?? 'origin',
                 entities: [
                     ...ExchangeEntities,
