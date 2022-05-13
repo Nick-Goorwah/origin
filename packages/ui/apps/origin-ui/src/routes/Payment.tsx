00934b91e9ecc2da8d0a048a6ccd0be92b2b0719
Nicholas Goorwah <Nick@VolterraTechnology.com>
2022-05-13 08:41:32 -0400

    Routes File

---
+++ packages/ui/apps/origin-ui/src/routes/Payment.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { LoginApp, LoginAppProps } from '@energyweb/origin-ui-user-view';
+
+const PaymentRoute = (props: LoginAppProps) => {
+  return <LoginApp {...props} />;
+};
+
+export default PaymentRoute;
